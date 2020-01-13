1082ed7b8c277bd83741a4e6f56c35874e50377a
Cecilia Sousa <ceciliasousa21@gmail.com>
2020-01-13 16:52:29 -0300

    initial commit

---
+++ Aula01/app/src/main/java/br/com/cecilia/aula01_layout/Login_Activity.kt
@@ -0,0 +1,12 @@
+package br.com.cecilia.aula01_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
